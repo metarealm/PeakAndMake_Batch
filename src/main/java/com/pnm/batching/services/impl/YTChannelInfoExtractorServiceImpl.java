2c401cb69e509e6df7835c2ad7f7b1403fbf51b3
bhabani.das@gmail.com <bhabani.das@gmail.com>
2018-04-28 20:37:40 -0700

    Multi query data processing added

--- src/main/java/com/pnm/batching/services/impl/YTChannelInfoExtractorServiceImpl.java
+++ src/main/java/com/pnm/batching/services/impl/YTChannelInfoExtractorServiceImpl.java
@@ -1,5 +1,6 @@
 package com.pnm.batching.services.impl;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -19,37 +20,48 @@ import com.pnm.batching.services.YTInfoExtractorService;
 @Service
 public class YTChannelInfoExtractorServiceImpl<E> extends YTInfoExtractorService {
 
+	private int maxQueryLoop = 10;
+
 	@Override
 	public HashSet<IYouTubeDTO> getYouTubeInfo() {
 		return null;
 	}
 
 	public List<YTChannelDto> getJsonData() {
+		List<YTChannelDto> channelData = new ArrayList<>();
 		try {
 			Optional<String> pageToken = Optional.empty();
-			String sPageToken = null;
-			
+
 			YouTube.Search.List searchListByKeywordRequest = this.youtubeService.search().list("snippet");
 			searchListByKeywordRequest.setMaxResults(10L);
 			searchListByKeywordRequest.setQ("indian recipe");
 			searchListByKeywordRequest.setType("channel");
-			searchListByKeywordRequest.setPageToken(sPageToken);
-			SearchListResponse response = searchListByKeywordRequest.execute();
-			
-			System.out.println("**************response*************");
-			// System.out.println(response.toPrettyString());
-			ObjectMapper mapper = new ObjectMapper();
-			String itemString = mapper.readTree(response.toString()).get("items").toString();
-			YTChannelDto[] channelData = mapper.readValue(itemString, YTChannelDto[].class);
-			System.out.println("**************json*************");
-			Stream.of(channelData).forEach(data -> {
+
+			for (int i = 0; i < maxQueryLoop; i++) {
+				if (pageToken.isPresent()){
+					searchListByKeywordRequest.setPageToken(pageToken.get());
+					System.out.println("current page token is "+ pageToken.get());
+				}
+				SearchListResponse response = searchListByKeywordRequest.execute();
+				System.out.println("**************response*************");
+				// System.out.println(response.toPrettyString());
+				ObjectMapper mapper = new ObjectMapper();
+				pageToken =Optional.of(mapper.readTree(response.toString()).get("nextPageToken").asText());
+				String itemString = mapper.readTree(response.toString()).get("items").toString();
+				System.out.println("*****************" + pageToken + "******************");
+				System.out.println(itemString);
+				channelData.addAll(Arrays.asList(mapper.readValue(itemString, YTChannelDto[].class)));
+				System.out.println("**************json*************");
+			}
+
+			channelData.forEach(data -> {
 				try {
 					System.out.println(new ObjectMapper().writeValueAsString(data));
 				} catch (JsonProcessingException e) {
 					e.printStackTrace();
 				}
 			});
-			return Arrays.asList(channelData);
+			return channelData;
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			System.out.println("Got exception " + ex.getMessage());
